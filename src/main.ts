d1fe587153ac55c72159a5b493e77c2d6292bc64
kaleb22 <kaleb22@hotmail.com>
2025-01-02 15:07:17 -0300

    chore: migrating to standalone apis to bootstrap the project

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, FormsModule, ReactiveFormsModule),
+        provideHttpClient(withInterceptorsFromDi()),
+        provideAnimations(),
+        provideRouter(routes)
+    ]
+})
   .catch(err => console.error(err));
